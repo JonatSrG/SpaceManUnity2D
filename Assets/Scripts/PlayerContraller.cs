ec4295536393e848fa4e556346d4812964d6b745
JonatSrG <JonatSrG>
2022-09-28 20:10:47 -0500

    Programando

---
+++ Assets/Scripts/PlayerContraller.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerContraller : MonoBehaviour {
+
+    //Variables del mov del personaje
+    public float jumpForce = 6f;
+    Rigidbody2D = rigidBody;
+
+    void Awake() {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKey(KeyCode.Space) || Input.GetMouseButtonDown(0)){
+            Jump();
+        }
+        
+    }
+
+    void Jump() {
+        rigidBody.AddForce(Vector2.up*jumpForce, ForceMode2D.Impulse);
+    }
+}
