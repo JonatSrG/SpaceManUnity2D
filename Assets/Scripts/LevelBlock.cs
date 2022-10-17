47cf532a39392ff80f1aa0a65ab497dc18fbc5de
JonatSrG <JonatSrG>
2022-10-16 20:28:19 -0500

    Generando precedural nivel

--- Assets/Scripts/LevelBlock.cs
+++ Assets/Scripts/LevelBlock.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class LevelBlock : MonoBehaviour
 {
     public Transform startPoint, existPoint;
-    
+
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,4 +19,6 @@ public class LevelBlock : MonoBehaviour
     {
         
     }
+
+    
 }
---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+
+    public  static LevelManager sharedInstance;
+
+    public List<LevelBlock> allTheLevelBlocks = new List<LevelBlock>();
+
+    public List<LevelBlock> currentLevelBlocks = new List<LevelBlock>();
+
+    public Transform levelStartPosition;
+
+    void Awake() {
+
+        if ( sharedInstance == null ){
+            sharedInstance = this;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GenerateInitialBlock();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddLevelBlock(){
+
+    }
+
+    public void RemoveLevelBlock(){
+
+    }
+
+    public void RemoveAllLevelBlock() {
+
+    }
+
+    public void GenerateInitialBlock() {
+
+        for (int i = 0; i < 2; i++)
+   {
+            AddLevelBlock();
+        }
+    }
+}
